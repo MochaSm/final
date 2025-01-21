bef766a7c807a1fac9e93555429d47953e0d4d4a
MochaSm <130510590+MochaSm@users.noreply.github.com>
2025-01-21 10:21:01 -0800

    stuff

--- mainscript.js
+++ mainscript.js
@@ -1,8 +1,11 @@
+//TODO: Add mmedia quearies for credits and add lives and maybe a loading screen
+
+
 const apikey ='JKiV4XYc0msceTYMlAI0meyijD64OdWTu77JCD8vNXVAWLpYPtU6DqvU'
 const easy = `https://opentdb.com/api.php?amount=20&difficulty=easy`
 const medium = `https://opentdb.com/api.php?amount=20&difficulty=medium`
 const hard =`https://opentdb.com/api.php?amount=20&difficulty=hard`
-
+let interval = null;
     let contain = document.createElement('div')
     let btncontain = document.createElement('div')
     let gear = document.getElementById('gear')
@@ -14,10 +17,33 @@ let data  = ''
 let i = 0;
 let x = '0'
 let y = '0'
+
+let amount = '10'
+let category = '9';
+let type = "multiple";
+
+//var changes
+function varchange(value){
+    amount = value
+  }
+  
+
+  
+  function varchange2(value){
+    category = value
+  
+  }
+  
+  function varchange3(value){
+    type = value
+  
+  }
+  
+
+
 onmousemove = function(e){
 let pos = document.getElementById('pos')
 
-    // console.log("mouse location:", e.clientX, e.clientY)
      x = e.clientX 
      y = e.clientY
     pos.innerHTML = 'x' +x +'y'+ y
@@ -28,21 +54,17 @@ function back(){
     document.getElementById('landing').style.display = 'grid'
     document.getElementById('setts').style.display = 'none'
     document.getElementById('creds').style.display = 'none'
+    document.getElementById('container').style.display = 'none'
 }
 
 function settings(){
     document.getElementById('landing').style.display = 'none'
     document.getElementById('setts').style.display = 'grid'
-
-    
-
 }
 
 function creds(){
     document.getElementById('landing').style.display = 'none'
     document.getElementById('creds').style.display = 'grid'
-
-
 }
 let lives = null;
 
@@ -52,14 +74,10 @@ let mulitplyer = null;
 
 
 let diff =null;
+let right = 0;
 
 // let mulitplyer  = lives * mult;
 
-window.onload = function() {
-
- 
-} // window.onload
- 
 
 
 // let = amount;
@@ -68,8 +86,9 @@ window.onload = function() {
 
 
 async function imgDelivery(data) { 
-        let contain = document.querySelector('.container')   
+        let contain = document.getElementById('container')   
         const query = `${data.results[i].question}`
+            console.log(i)
             console.log(query);
         const imageUrl = await fetchPexelsData(query);
 
@@ -118,13 +137,14 @@ async function fetchPexelsData(search) {
 }
 
 async function searchtrivea(diff) {
+    temp = ''
     diff = diff;
     document.getElementById('landing').style.display = 'none'
 
     try {   
-        // const response = await fetch(`https://opentdb.com/api.php?amount=20&difficulty=${diff}&type=multiple`);
+        const response = await fetch(`https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${diff}&type=${type}`);
         
-        const response = await fetch('/questions.json');
+        // const response = await fetch('/questions.json');
         data = await response.json();
         console.log(data);
         show(data)
@@ -137,93 +157,96 @@ async function searchtrivea(diff) {
   } // searchTvShows 
 
 function show(data) {
+    clearInterval(interval);
+
     contain.innerHTML = ''
     btncontain.innerHTML = ''
 
-    if(data.results[i].difficulty == 'hard'){
-        duration = 5
-        lives=10
-        mulitplyer = lives*Math.floor(Math.random() * .90)
-        console.log(mulitplyer)
-    }else if(data.results[i].difficulty == 'medium'){
-        lives=25
-        console.log(lives)
-        mulitplyer = lives*(Math.random() * .90)
-        console.log(mulitplyer)
-        duration = 10
-
-    }else if(data.results[i].difficulty == 'easy'){
-        lives=50
-        console.log(lives) 
-        mulitplyer = lives*Math.floor(Math.random() * .90)
-        console.log(mulitplyer)
-        duration = 15
-
-    }
+   
     if(data.results.length > i){
-        console.log(data.results.length)
-    console.log(i)
-
-    let answer = document.getElementById('questions')
-    let timer = document.createElement('div'
-        
-    )
-    timer.setAttribute('id', 'timer');
-    btncontain.classList.add('btnconatin')
-    contain.classList.add('container')
-    contain.appendChild(btncontain)
-    answer.appendChild(contain)
-    console.log(data.results[i].question ) 
-    let Div = document.createElement("div");
-    Div.classList.add('question')
-    Div.innerHTML = "<h2>"+data.results[i].question+"</h2>";
-    contain.appendChild(Div)
+        if(data.results[i].difficulty == 'hard'){
+            duration = 5
+            lives=10
+            mulitplyer = lives*Math.floor(Math.random() * .90)
+            console.log(mulitplyer)
+        }else if(data.results[i].difficulty == 'medium'){
+            lives=25
+            console.log(lives)
+            mulitplyer = lives*(Math.random() * .90)
+            console.log(mulitplyer)
+            duration = 10
     
-    const correctAnswer = data.results[i].correct_answer;
-    contain.appendChild(timer)
-
-    startTimer(duration,timer)
-
-
-    if (data.results[i].type === 'boolean') {
-        for(let j = 0; j < 2; j++){
-            
-            if(j == 0){
-                let btns = document.createElement('button')
-                btncontain.appendChild(btns)
-                btns.innerHTML = 'true'
-
-                btns.addEventListener('click', function() {
-                    checkAnswer(answerText, correctAnswer);
-                });
-
-            }
-            else{
-                let btns = document.createElement('button')
-                btncontain.appendChild(btns)
-                btns.innerHTML = 'false'
-
-                btns.addEventListener('click', function() {
-                    checkAnswer(answerText, correctAnswer);
-                });
-            }
+        }else if(data.results[i].difficulty == 'easy'){
+            lives=50
+            console.log(lives) 
+            mulitplyer = lives*Math.floor(Math.random() * .90)
+            console.log(mulitplyer)
+            duration = 15
+    
         }
-    } else if (data.results[i].type === 'multiple') {
-        const allAnswers = [...data.results[i].incorrect_answers, correctAnswer];
-        allAnswers.sort(function() { return 0.5 - Math.random(); });
+        console.log(data.results.length)
+        console.log(i)
 
-        for (let j = 0; j < allAnswers.length; j++) {
-            createAnswer(allAnswers[j], correctAnswer);
-        }
-    }
+        let answer = document.getElementById('questions')
+        let timer = document.createElement('div' )
+        timer.setAttribute('id', 'timer');
+        btncontain.classList.add('btnconatin')
+        contain.setAttribute('id', 'container')
+        contain.appendChild(btncontain)
+        answer.appendChild(contain)
+        console.log(data.results[i].question ) 
+        let Div = document.createElement("div");
+        Div.classList.add('question')
+        Div.innerHTML = "<h2>"+data.results[i].question+"</h2>";
+        contain.appendChild(Div)
+        
+        const correctAnswer = data.results[i].correct_answer;
+        contain.appendChild(timer)
+
+        startTimer(duration,timer)
+
+
+        if (data.results[i].type === 'boolean') {
+            for(let j = 0; j < 2; j++){
+                
+                if(j == 0){
+                    let btns = document.createElement('button')
+                    btncontain.appendChild(btns)
+                    btns.innerHTML = 'True'
+                    answerText = 'True'
+
+                    btns.addEventListener('click', function() {
+                        checkAnswer(answerText, correctAnswer);
+                    });
+
+                }
+                else{
+                    let btns = document.createElement('button')
+                    btncontain.appendChild(btns)
+                    btns.innerHTML = 'False'
+                    answerText = 'False'
+                    
+
+
+                    btns.addEventListener('click', function() {
+                        checkAnswer(answerText, correctAnswer);
+                    });
+                }
+            }
+            } else if (data.results[i].type === 'multiple') {
+                const allAnswers = [...data.results[i].incorrect_answers, correctAnswer];
+                allAnswers.sort(function() { return 0.5 - Math.random(); });
+
+                for (let j = 0; j < allAnswers.length; j++) {
+                    createAnswer(allAnswers[j], correctAnswer);
+                }
+            }
     }else{
-        contain.innerHTML = '<p>hey</p>'
-
+        contain.innerHTML = `<h1>game over stinky</h1> \n <p>you got ${right}/${amount} </p>   <i id="back" onclick="back()" class="fa-solid fa-arrow-left"></i>`
+        i = 0
     }
     
 }
-    // questionDiv.appendChild(answersList);
-    // answersContainer.appendChild(questionDiv)
 
     function createAnswer(answerText, correctAnswer) {
 
@@ -245,15 +268,23 @@ function show(data) {
         if(answerText == correctAnswer){
             show(data)
             imgDelivery(data)
-
+            right++
             i++
 
-            console.log('yup')
         }else{
-            console.log(mulitplyer)
+            i++
+
+            contain.innerHTML = ''
             lives = lives - mulitplyer;
-            
-            console.log(lives)
+            let temp = document.createElement('div')
+            temp.classList.add('temp')
+            temp.innerHTML = `<h1  style="color: red; font-size: 2.5em;">WRONG</h1> \n <h2>you have ${lives} lives left</h2>`
+            contain.appendChild(temp)
+
+            setTimeout(() => {
+                show(data)
+                temp.innerHTML = ''
+            }, 3000); 
 
         }
     }else{
@@ -262,21 +293,18 @@ function show(data) {
     }
 
     function startTimer(duration, timer) {
-        let times = duration, minutes, seconds;
+        let times = duration, seconds;
         let startTime = Date.now() + 1000; 
-    
         interval = setInterval(() => {
-            minutes = parseInt(times / 60, 10);
             seconds = parseInt(times % 60, 10);
+        console.log(seconds)
             
-            if (minutes < 10) {
-                minutes = "0" + minutes;
-            }
+            
             if (seconds < 10) {
                 seconds = "0" + seconds;
             }
             
-            document.getElementById('timer').innerHTML = '<i class="fa-regular fa-clock"></i>' + minutes + ":" + seconds;
+            document.getElementById('timer').innerHTML = '<i class="fa-regular fa-clock"></i>' + seconds +' seconds'   ;
             if (--times < 0) {
                 times = 0;
                 gameover();
@@ -287,7 +315,8 @@ function show(data) {
     }
 
     function gameover(){
-        contain.innerHTML = 'game voer stinky'
+        contain.innerHTML = `<h1>game over stinky</h1> \n <p>you got ${right}/${amount} </p>         <i id="back" onclick="back()" class="fa-solid fa-arrow-left"></i>`
+        i = 0
 
     }
    
@@ -300,7 +329,7 @@ function show(data) {
       deferredPrompt = e;
     
       const installButton = document.getElementById('installButton');
-      installButton.style.display = 'block';
+      installButton.style.display = 'inline-block';
     
       installButton.addEventListener('click', () => {
         console.log('what the sigma')
@@ -308,12 +337,23 @@ function show(data) {
         deferredPrompt.prompt();
         deferredPrompt.userChoice.then((choiceResult) => {
           if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the install prompt');
-          } else {
+                console.log('User accepted the install prompt');
+            } else {
             console.log('User dismissed the install prompt');
           }
           deferredPrompt = null;
         });
       });
+      
     });                    
              
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function() {
+          navigator.serviceWorker.register('/sw.js').then(function(registration) {
+            console.log('Service Worker registered with scope:', registration.scope);
+          }, function(error) {
+            console.log('Service Worker registration failed:', error);
+          });
+        });
+      }                    
+        
